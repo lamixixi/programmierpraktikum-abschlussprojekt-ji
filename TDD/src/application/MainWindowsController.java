8d26ce4362dc49f793d83899306f05f8eac148c6
saiyinwuchari <saiyinwuchari.saiyinwuchari@uni-duesseldorf.de>
2016-07-10 10:06:20 +0200

    Hier ist das Countdown

--- TDD/src/application/MainWindowsController.java
+++ TDD/src/application/MainWindowsController.java
@@ -96,6 +96,19 @@ public class MainWindowsController implements Initializable {
 	
 	@FXML
 	public void Erweiterung1(ActionEvent event) { // Butto fur Erweiterung 1
+		
+		try {
+			//((Node) event.getSource()).getScene().getWindow().hide();
+			Stage primaryStage = new Stage();
+			Parent root = FXMLLoader.load(getClass().getResource("/application/BadySept.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Red");
+			primaryStage.show();
+		} catch (Exception e) {
+
+		}
 
 	}
 
